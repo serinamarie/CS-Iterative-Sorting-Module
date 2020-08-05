1ddecb7fc4db96820ba5839aece07b3f9b7ac16c
Serina Grill <gril0044@umn.edu>
2020-08-05 19:18:35 -0400

    module 1 complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,54 @@
 def linear_search(arr, target):
-    # Your code here
 
+    # for item in the range of the length of the array
+    for i in range(0, len(arr)):
 
-    return -1   # not found
+        # if the value is equal to the target
+        if arr[i] == target:
+
+            # return the array's index
+            return i
+
+    # if not found 
+    return -1 
 
 
 # Write an iterative implementation of Binary Search
-def binary_search(arr, target):
+def binary_search(arr, target, low=0, high=None):
 
-    # Your code here
+    # if empty array
+    if len(arr) == 0:
+        return -1
+    
+    # if no high value has been set
+    if high == None:
+        # the high index is the length of the array - 1
+        high = len(arr) - 1
 
+    # create the midpoint 
+    midpoint = (high + low) // 2
 
-    return -1  # not found
+    # if the midpoint value is the target
+    if arr[midpoint] == target:
+        return midpoint
+
+    # if the midpoint value is less than the target
+    elif arr[midpoint] < target: 
+        # set a new low 
+        low = midpoint + 1
+
+        # then run through again
+        return binary_search(arr, target, low, high)
+
+    # if the midpoint value is mire than the target
+    elif arr[midpoint] > target:
+
+        # set a new high 
+        high = midpoint - 1 
+
+        # then run through again
+        return binary_search(arr, target, low, high)
+
+    # if the midpoint value is none of these
+    else:
+        return -1  # not found
